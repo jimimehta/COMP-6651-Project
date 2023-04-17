95ef9885cddaa693e2580a16e3d8edfd6f67d08b
Jimi Mehta <99949870+jimimehta@users.noreply.github.com>
2023-04-16 22:37:34 -0400

    Update graph.py

--- graph.py
+++ graph.py
@@ -2,6 +2,7 @@ import networkx as nx
 import random
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import os
 
 from bokeh.io import show, output_notebook
 from bokeh.plotting import figure
@@ -16,12 +17,14 @@ import time
 
 import pandas as pd
 
+#Color map for each color assigned to specific value
 colormap = {
     1:"blue",2:"red",3:"green",4:"orange",5:"purple",6:"brown",7:"pink",8:"gray",9:"olive",10:"cyan",11:"black",
     12:"yellow",13:"lightcoral",14:"maroon",15:"sienna",16:"peru",17:"tan",18:"gold",19:"darkkhaki",20:"olive",
     21:"lime",22:"turquoise",23:"teal",24:"midnightblue",25:"darkviolet",26:"fuchsia"
 }
 
+#Fuction to sort Graph Vertices on the basis of Degree of Vertex
 def sort_by_func(lst, func):
     tuples = [(val, func(val)) for val in lst]
     # Sort the tuples based on the second element (i.e. the function output).
@@ -29,6 +32,7 @@ def sort_by_func(lst, func):
     # Return a new list containing the first element of each tuple (i.e. the original values) in the sorted order.
     return [tup[0] for tup in sorted_tuples]
 
+#First-Fit algorithm to color traingle-free graph in an online fashion
 def firstFit(graph, colormap):
     og_nodes = graph.nodes()
     #nodes = sort_by_func(og_nodes, graph.degree)
@@ -45,7 +49,7 @@ def firstFit(graph, colormap):
     node_color = {vert: colormap[1]}  # Node 3 will be red
     nx.set_node_attributes(nG, node_color, 'color')
     gc_map[vert]= 1
-    
+    ggid=10
     while(len(nodes)>0):
         vert = nodes.pop(0)
         edgeso = nG.edges()
@@ -72,12 +76,15 @@ def firstFit(graph, colormap):
             start, end = edge
             nG.add_edge(start, end)
         added_nodes.append(vert)
+        plot_colored_v2(nG, ggid,gc_map)
+        ggid+=1
         #print nG graph here - to show the progress vertex by vertex
 #         print("----------------------------")
     return nG, gc_map 
 	
 	
 
+#Generate Traingle-Free Graph
 def generate_map(n,p):
     # create an empty graph
     G = nx.Graph()
@@ -97,13 +104,15 @@ def generate_map(n,p):
     return G
 	
 
-
+#Save Graph as Pickle
 def save_graph(G, gid):
     with open('Graph_'+str(gid), 'wb') as f:
         pickle.dump(G, f)
     return 'Graph_'+str(gid)
 	
 	
+    
+#Load Graph from Generate Pickle    
 def load_graph(pfile):
     with open(pfile, 'rb') as f:
         G = pickle.load(f)
@@ -111,6 +120,8 @@ def load_graph(pfile):
     return G, gid
 	
 
+    
+#Plot Normal Graph without coloring
 def plot_normal(G, gid):
     pos = nx.circular_layout(G)
 #     fig, ax = plt.subplots()
@@ -124,6 +135,7 @@ def plot_normal(G, gid):
     plt.clf()
 	
 
+#Plot Colored Graph using First-Fit Algorithm
 def plot_colored(G, gid):
     
     nG, gc_map = firstFit(G, colormap)
@@ -140,14 +152,23 @@ def plot_colored(G, gid):
 #     plt.axis("off")
 #     plt.show()
     plt.savefig('Colored_'+str(gid)+'.png', dpi=300, bbox_inches='tight')
-    return nG, gc_map
     plt.clf()
+    return nG, gc_map
+    
 	
 
+    
+#Use this Function to Generate First-Fit Colored Triangle-Free Graphs in Bulk
+#Change parameters as per your desire
+#default is 100 Graphs of 15 vertices
+#Create FirstFitStats.xlsx with column names as Graph Id, vertex_count, color_count or use the empty file given with code
 def run_experiment():
     df = pd.read_excel('FirstFitStats.xlsx')
-    gid = 1000
-    for i in range(25,26):
+    #gid is id number of generated graph
+    gid = 100
+    #range(n,n+1), n=number of vertices 
+    for i in range(15,16):
+        #Do not make changes to p if you want to generate 100 graphs of n vertices
         p=0.25
         n = i
         while p <= 0.40:
@@ -165,5 +186,136 @@ def run_experiment():
                 new_row = { 'Graph Id':filepath, 'vertex_count':n, 'color_count':colorCount }
                 df.loc[len(df)] = new_row
             p+=0.05
+            
+    df.to_excel('FirstFitStats.xlsx', index=False)    
+#Stat file of First-Fit is generated and written in FirstFitStats.xlsx    
 
-    df.to_excel('FirstFitStats.xlsx', index=False)
+
+
+def firstFit_UI(graph, colormap):
+    og_nodes = graph.nodes()
+    #nodes = sort_by_func(og_nodes, graph.degree)
+    nodes = list(og_nodes)
+    nG = nx.Graph()
+    all_colors = [i for i in range(1,27)]
+    gc_map = [-1 for i in range(0,len(nodes))]
+    #print(gc_map)
+    added_nodes = []
+    vert = nodes.pop(0)
+    nG.add_node(vert)
+    added_nodes.append(vert)
+    
+    node_color = {vert: colormap[1]}  # Node 3 will be red
+    nx.set_node_attributes(nG, node_color, 'color')
+    gc_map[vert]= 1
+    ggid=10
+    while(len(nodes)>0):
+        vert = nodes.pop(0)
+        edgeso = nG.edges()
+#         for e in edgeso:
+#             print(e)
+#         print("Processing vertex "+str(vert))
+        vert_edges = graph.edges(vert)
+        n_edges =[]
+        non_adjacent_colors = all_colors.copy() 
+#         print(non_adjacent_colors)
+        for edge in vert_edges:
+            source, end = edge
+            if end in added_nodes:
+                n_edges.append(edge)
+#                 print(gc_map[end])
+                if gc_map[end] in non_adjacent_colors:
+                    non_adjacent_colors.remove(gc_map[end])
+        vcolor = min(non_adjacent_colors)
+        node_color = {vert: colormap[vcolor]}
+        nG.add_node(vert)
+        nx.set_node_attributes(nG, node_color, 'color')
+        gc_map[vert]=vcolor
+        for edge in n_edges:
+            start, end = edge
+            nG.add_edge(start, end)
+        added_nodes.append(vert)
+        plot_colored_v3(nG, ggid,gc_map)
+        ggid+=1
+        #print nG graph here - to show the progress vertex by vertex
+#         print("----------------------------")
+    return nG, gc_map 
+
+def plot_colored_v2(nG, ggid, gc_map):
+    
+    pos = nx.circular_layout(nG)
+    nx.draw_networkx_nodes(nG, pos, node_size=500)
+    nx.draw_networkx_edges(nG, pos, width=1)
+    ng_nodes = list( nG.nodes() )
+    nGcolors = {i:colormap[gc_map[i]] for i in ng_nodes if gc_map[i]!=-1}
+    nx.draw(nG, pos=nx.circular_layout(nG), node_color=[nGcolors.get(node, 'k') for node in nG.nodes()])
+    plt.savefig('StepByStep_'+str(ggid)+'.png', dpi=300, bbox_inches='tight')
+    plt.clf()
+    
+def plot_colored_v3(nG, ggid, gc_map):
+    
+    pos = nx.circular_layout(nG)
+    nx.draw_networkx_nodes(nG, pos, node_size=500)
+    nx.draw_networkx_edges(nG, pos, width=1)
+    ng_nodes = list( nG.nodes() )
+    nGcolors = {i:colormap[gc_map[i]] for i in ng_nodes if gc_map[i]!=-1}
+    nx.draw(nG, pos=nx.circular_layout(nG), node_color=[nGcolors.get(node, 'k') for node in nG.nodes()])
+    plt.show('StepByStep_'+str(ggid)+'.png', dpi=300, bbox_inches='tight')
+    plt.clf()
+        
+def plot_colored_v4(G, gid):
+    
+    nG, gc_map = firstFit_UI(G, colormap)
+    pos = nx.circular_layout(nG)
+    nx.draw_networkx_nodes(nG, pos, node_size=500)
+    nx.draw_networkx_edges(nG, pos, width=1)
+    
+    #colors = ["r" if color_map[v] == 0 else "b" for v in G]
+#     print(gc_map)
+    ncolors = [colormap[gc_map[i]] for i in nG.nodes()]
+
+    nx.draw(nG, pos=nx.circular_layout(nG), node_color=ncolors)
+    #nx.draw_networkx_labels(nG, pos, font_size=20, font_family="sans-serif")
+#     plt.axis("off")
+#     plt.show()
+    plt.savefig('Colored_'+str(gid)+'.png', dpi=300, bbox_inches='tight')
+    plt.clf()
+    return nG, gc_map
+
+#To plot Single Graph images and pickle Step by Step    
+def run_experiment_v2():
+    gid = 100
+    p=0.35
+    #n = Number of Vertices
+    n = 10
+    G = generate_map(n,p)
+    gid = int(gid)
+    gid+=1
+    filepath = save_graph(G, gid)
+    time.sleep(1)
+    G, gid = load_graph(filepath)
+    plot_normal(G, gid)
+    nG, gc_map = plot_colored(G, gid) 
+    
+    
+#Use this Function to Duplicated experiment from the pickle files that are genrated using function run_experiment() or run_experiment_v2()
+def duplicate_experiment(folder_name):
+    
+    # replace with the name of the folder you want to list
+    folder_path = os.path.join(os.getcwd(), folder_name)
+    extension = "pickle"  # replace with the extension you want to filter
+    files = os.listdir(folder_path)
+    files = [f for f in os.listdir(folder_path) if f.endswith(extension)]
+    column_names = ['Graph Id', 'vertex_count', 'color_count']  
+    df = pd.DataFrame({}, columns=column_names)
+    for f in files:
+        file_path = os.path.join(folder_path, f)
+        G, gid = load_graph(file_path)
+        n=len(list(G.nodes()))
+        plot_normal(G, gid)
+        nG, gc_map = plot_colored(G, gid)
+        colorset = set(gc_map)
+        colorCount = len(colorset)
+        new_row = { 'Graph Id':f, 'vertex_count':n, 'color_count':colorCount }
+        df.loc[len(df)] = new_row
+    df.to_excel(folder_name+'-FirstFitStats.xlsx', index=False)       
