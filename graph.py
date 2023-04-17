eb846c6498895a909e7969d1d5dcff98d7e92f61
Jimi Mehta <99949870+jimimehta@users.noreply.github.com>
2023-04-16 23:42:58 -0400

    Update graph.py

--- graph.py
+++ graph.py
@@ -106,9 +106,9 @@ def generate_map(n,p):
 
 #Save Graph as Pickle
 def save_graph(G, gid):
-    with open('Graph_'+str(gid), 'wb') as f:
+    with open('Graph_'+str(gid)+'.pickle', 'wb') as f:
         pickle.dump(G, f)
-    return 'Graph_'+str(gid)
+    return 'Graph_'+str(gid)+'.pickle'
 	
 	
     
@@ -165,7 +165,7 @@ def plot_colored(G, gid):
 def run_experiment():
     df = pd.read_excel('FirstFitStats.xlsx')
     #gid is id number of generated graph
-    gid = 100
+    gid = 2200
     #range(n,n+1), n=number of vertices 
     for i in range(15,16):
         #Do not make changes to p if you want to generate 100 graphs of n vertices
@@ -192,55 +192,6 @@ def run_experiment():
 
 
 
-def firstFit_UI(graph, colormap):
-    og_nodes = graph.nodes()
-    #nodes = sort_by_func(og_nodes, graph.degree)
-    nodes = list(og_nodes)
-    nG = nx.Graph()
-    all_colors = [i for i in range(1,27)]
-    gc_map = [-1 for i in range(0,len(nodes))]
-    #print(gc_map)
-    added_nodes = []
-    vert = nodes.pop(0)
-    nG.add_node(vert)
-    added_nodes.append(vert)
-    
-    node_color = {vert: colormap[1]}  # Node 3 will be red
-    nx.set_node_attributes(nG, node_color, 'color')
-    gc_map[vert]= 1
-    ggid=10
-    while(len(nodes)>0):
-        vert = nodes.pop(0)
-        edgeso = nG.edges()
-#         for e in edgeso:
-#             print(e)
-#         print("Processing vertex "+str(vert))
-        vert_edges = graph.edges(vert)
-        n_edges =[]
-        non_adjacent_colors = all_colors.copy() 
-#         print(non_adjacent_colors)
-        for edge in vert_edges:
-            source, end = edge
-            if end in added_nodes:
-                n_edges.append(edge)
-#                 print(gc_map[end])
-                if gc_map[end] in non_adjacent_colors:
-                    non_adjacent_colors.remove(gc_map[end])
-        vcolor = min(non_adjacent_colors)
-        node_color = {vert: colormap[vcolor]}
-        nG.add_node(vert)
-        nx.set_node_attributes(nG, node_color, 'color')
-        gc_map[vert]=vcolor
-        for edge in n_edges:
-            start, end = edge
-            nG.add_edge(start, end)
-        added_nodes.append(vert)
-        plot_colored_v3(nG, ggid,gc_map)
-        ggid+=1
-        #print nG graph here - to show the progress vertex by vertex
-#         print("----------------------------")
-    return nG, gc_map 
-
 def plot_colored_v2(nG, ggid, gc_map):
     
     pos = nx.circular_layout(nG)
@@ -252,35 +203,7 @@ def plot_colored_v2(nG, ggid, gc_map):
     plt.savefig('StepByStep_'+str(ggid)+'.png', dpi=300, bbox_inches='tight')
     plt.clf()
     
-def plot_colored_v3(nG, ggid, gc_map):
-    
-    pos = nx.circular_layout(nG)
-    nx.draw_networkx_nodes(nG, pos, node_size=500)
-    nx.draw_networkx_edges(nG, pos, width=1)
-    ng_nodes = list( nG.nodes() )
-    nGcolors = {i:colormap[gc_map[i]] for i in ng_nodes if gc_map[i]!=-1}
-    nx.draw(nG, pos=nx.circular_layout(nG), node_color=[nGcolors.get(node, 'k') for node in nG.nodes()])
-    plt.show('StepByStep_'+str(ggid)+'.png', dpi=300, bbox_inches='tight')
-    plt.clf()
-        
-def plot_colored_v4(G, gid):
-    
-    nG, gc_map = firstFit_UI(G, colormap)
-    pos = nx.circular_layout(nG)
-    nx.draw_networkx_nodes(nG, pos, node_size=500)
-    nx.draw_networkx_edges(nG, pos, width=1)
-    
-    #colors = ["r" if color_map[v] == 0 else "b" for v in G]
-#     print(gc_map)
-    ncolors = [colormap[gc_map[i]] for i in nG.nodes()]
 
-    nx.draw(nG, pos=nx.circular_layout(nG), node_color=ncolors)
-    #nx.draw_networkx_labels(nG, pos, font_size=20, font_family="sans-serif")
-#     plt.axis("off")
-#     plt.show()
-    plt.savefig('Colored_'+str(gid)+'.png', dpi=300, bbox_inches='tight')
-    plt.clf()
-    return nG, gc_map
 
 #To plot Single Graph images and pickle Step by Step    
 def run_experiment_v2():
@@ -300,10 +223,9 @@ def run_experiment_v2():
     
 #Use this Function to Duplicated experiment from the pickle files that are genrated using function run_experiment() or run_experiment_v2()
 def duplicate_experiment(folder_name):
-    
     # replace with the name of the folder you want to list
     folder_path = os.path.join(os.getcwd(), folder_name)
-    extension = "pickle"  # replace with the extension you want to filter
+    extension = ".pickle"  # replace with the extension you want to filter
     files = os.listdir(folder_path)
     files = [f for f in os.listdir(folder_path) if f.endswith(extension)]
     column_names = ['Graph Id', 'vertex_count', 'color_count']  
@@ -318,4 +240,5 @@ def duplicate_experiment(folder_name):
         colorCount = len(colorset)
         new_row = { 'Graph Id':f, 'vertex_count':n, 'color_count':colorCount }
         df.loc[len(df)] = new_row
-    df.to_excel(folder_name+'-FirstFitStats.xlsx', index=False)       
+    df.to_excel(folder_name+'-FirstFitStats.xlsx', index=False)   
+    
